2cd245a162c0d8ffd84d96e76b16885d5d309ba9
mouhamed <mouhamed.mbaye1@outlook.com>
2023-09-03 22:36:35 +0200

    added burger menu on responsive max-width 768px

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,13 @@
+
 import { Link } from "react-router-dom"
 import '../css/header.css'
 import logo from '../assets/hrnetLogo.png'
+import bgMenu from '../assets/burger-menu.svg'
+import { useState } from "react"
 
 
 function Header() {
+    const [display, setDisplay] = useState(false)
 
     return (
 
@@ -21,6 +25,34 @@ function Header() {
                 <Link to='/' className='current-employees'>
                     Create Employee
                 </Link>
+            </div>
+            <div className="burger-menu">
+                <img
+                    id='menu'
+                    src={bgMenu}
+                    alt="bouton menu du site"
+                    onClick={() => {
+                        setDisplay(!display)
+                    }}
+                />
+                <ul className={display ? 'list display' : 'list'}>
+                    <Link
+                        to='/employees'
+                        onClick={() => {
+                            setDisplay(!display)
+                        }}
+                    >
+                        Employees List
+                    </Link>
+                    <Link
+                        to='/'
+                        onClick={() => {
+                            setDisplay(!display)
+                        }}
+                    >
+                        Create an Employee
+                    </Link>
+                </ul>
             </div>
         </section>
     )
